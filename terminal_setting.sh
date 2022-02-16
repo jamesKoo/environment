568e0b3389a73140874798b1bdd3a3eea9764748
Jamyoung Koo <jmkoo@si-analytics.ai>
2022-02-16 18:17:00 +0900

    init commit

--- terminal_setting.sh
+++ terminal_setting.sh
@@ -5,6 +5,6 @@ cp -f .zshrc .p10k.zsh ~
 chsh -s $(which zsh)
 
 wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
-dpkg -i sd_0.21.0_amd64.deb
+dpkg -i lsd_0.21.0_amd64.deb
 
 zsh
