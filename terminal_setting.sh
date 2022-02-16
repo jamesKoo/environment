43e0c6fb36ead67c69875dfd2f3887288b3f94a0
Jamyoung Koo <jmkoo@si-analytics.ai>
2022-02-16 18:14:23 +0900

    init commit

--- terminal_setting.sh
+++ terminal_setting.sh
@@ -1,4 +1,6 @@
-sh -c "$(curl -fsSL https://git.io/zinit-install)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#!/bin/zsh
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 cp -f .zshrc .p10k.zsh ~
+chsh -s $(which zsh)
+zsh
