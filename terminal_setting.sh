05aa33e5aaa3680ba462ce90d1883bc44a07fa52
Jamyoung Koo <jmkoo@si-analytics.ai>
2022-02-16 18:16:03 +0900

    init commit

--- terminal_setting.sh
+++ terminal_setting.sh
@@ -3,4 +3,8 @@
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 cp -f .zshrc .p10k.zsh ~
 chsh -s $(which zsh)
+
+wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
+dpkg -i sd_0.21.0_amd64.deb
+
 zsh
