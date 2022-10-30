190d9fdb4241a31706205285a9119062e8e199c7
Jamyoung Koo <jmkoo@Jakeoungs-MacBook-Pro.local>
2022-10-30 19:17:08 +0900

    install lsd for mac

--- terminal_setting.sh
+++ terminal_setting.sh
@@ -4,7 +4,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 cp -f .zshrc .p10k.zsh ~
 chsh -s $(which zsh)
 
-wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
-dpkg -i lsd_0.21.0_amd64.deb
-
-zsh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install lsd
+else
+	wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
+	dpkg -i lsd_0.21.0_amd64.deb
+fi
+zsh	
